462a0ed794de5e0fecd65095d59e3ff16dfe21b9
klxcoder <klxcoder@gmail.com>
2025-03-27 20:42:13 +0700

    tensor1d

--- 02/script.js
+++ 02/script.js
@@ -1,4 +1,11 @@
-const tensor = tf.scalar(30);
+{
+  const tensor = tf.scalar(30);
+  console.log(tensor);
+  tensor.print();
+}
 
-console.log(tensor);
-tensor.print();
+{
+  const tensor = tf.tensor1d([1, 2, 3]);
+  console.log(tensor);
+  tensor.print();
+}
