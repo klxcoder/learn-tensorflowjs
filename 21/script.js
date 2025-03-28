c3cb2055e1b772d5e9f696fd8d703b97338b0f18
klxcoder <klxcoder@gmail.com>
2025-03-28 22:27:24 +0700

    fisherYatesShuffle replace tf.util.shuffleCombo

--- 21/script.js
+++ 21/script.js
@@ -7,3 +7,12 @@ console.log('INPUTS = ', INPUTS)
 const OUTPUTS = TRAINING_DATA.outputs;
 
 console.log('OUTPUTS = ', OUTPUTS)
+
+function fisherYatesShuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1)); // Random index within range
+    [array[i], array[j]] = [array[j], array[i]];   // Swap elements
+  }
+}
+
+fisherYatesShuffle([INPUTS, OUTPUTS])
